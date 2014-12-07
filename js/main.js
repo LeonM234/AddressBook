25c5bcbf0afd4a1889c148607b46c8f9b62f015b
Leon Peck <leonpeck234@gmail.com>
2014-12-07 17:26:09 -0600

    Add addresses.  WIP: Posting to firebase

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 ;(function(){
   'use strict';
 
-  angular.module('addressBook', []);
+  angular.module('addressBook', [])
     .controller('addressBookController', function($http){
       var vm = this;
 
@@ -11,14 +11,30 @@
         });
 
       vm.addNewContact = function(){
-        vm.contacts.push(vm.newContact);
+        $http.post('https://leonaddress-book.firebaseio.com/contacts.json')
+          .success(function(data){
+            vm.contacts[data.name] = vm.newContact;
+            vm.newContact = null;
+            console.log("add new contact is firing. new info is:" + vm.newContact.name);
+          });
+          .error(function(err){
+            console.log(err);
+          });
+        // --Prev Code--
+        // vm.contacts.push(vm.newContact);
       };
 
       vm.removeContact = function(contact){
-        var index = vm.contacts.indexOf(contact);
-        vm.contacts.splice(index, 1);
+        var url = 'https://leonaddress-book.firebaseio.com/' + addId + '.json';
+        $http.delete(url)
+          .success(function(){
+            delete vm.contacts[add];
+          });
+        // --Prev Code--
+        // var index = vm.contacts.indexOf(contact);
+        // vm.contacts.splice(index, 1);
       };
 
-      vm.newContact = null;
+      vm.newContact = {};
   });
 })();
