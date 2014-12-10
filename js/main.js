18a47705bd076e9eb7ff623dea8330ea115c7046
Leon Peck <leonpeck234@gmail.com>
2014-12-10 13:14:03 -0600

    Factories implemented

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 ;(function(){
   'use strict';
 
+  // ----- MODULE -----
   angular.module('addressBook', ['ngRoute', 'mgcrea.ngStrap'])
     .config(function($routeProvider){
       $routeProvider
@@ -27,76 +28,115 @@
         .otherwise({redirectTo: '/'});
     })
 
-    .controller('ShowController', function($http, $routeParams){
-      var vm = this;
-      var id = $routeParams.id;
-      $http.get('https://leonaddress-book.firebaseio.com/contacts/' + id + '.json')
+    // ----- FACTORY -----
+    .factory('contactsFactory', function($http, $location){
+
+      function getContact(id, cb){
+        var url = 'https://leonaddress-book.firebaseio.com/contacts/' + id + '.json'
+        $http.get(url)
         .success(function(data){
-          console.log(data);
-          vm.contact = data;
+          cb(data);
         })
         .error(function(err){
           console.log(err);
-        });
-    })
-
-    .controller('EditController', function($http, $routeParams, $location){
-      var vm = this;
-      var id = $routeParams.id;
-      var url = 'https://leonaddress-book.firebaseio.com/contacts/' + id + '.json'
-      $http.get(url)
-        .success(function(data){
-          vm.newContact = data;
-        })
-        .error(function(err){
-          console.log(err);
-        });
-
-      vm.addNewContact = function(){
-        $http.put(url, vm.newContact)
-          .success(function(data){
-            $location.path('/')
-          })
-          .error(function(err){
-            console.log(err);
-          });
-        };
-    })
-
-    .controller('addressBookController', function($http){
-      var vm = this;
-
-      var httpget = function(){
-        $http.get('https://leonaddress-book.firebaseio.com/contacts.json')
-        .success(function(data){
-          vm.contacts = data;
         });
       }
 
-      httpget();
+      function editContact(id, contact){
+        var url = 'https://leonaddress-book.firebaseio.com/contacts/' + id + '.json'
+        $http.put(url, contact)
+        .success(function(data){
+          $location.path('/')
+        })
+        .error(function(err){
+          console.log(err);
+        });
+      }
+
+      function getAllContacts(cb){
+        $http.get('https://leonaddress-book.firebaseio.com/contacts.json')
+        .success(function(data){
+          cb(data)
+        })
+        .error(function(err){
+          console.log(err);
+        });
+      }
+
+      function createContact(contact, cb){
+        $http.post('https://leonaddress-book.firebaseio.com/contacts.json', contact)
+        .success(function(data){
+          cb(data)
+        })
+        .error(function(err){
+          console.log('add contact error:' + err);
+        });
+      }
+
+      function deleteContact(contactId, cb){
+        var url = 'https://leonaddress-book.firebaseio.com/contacts/' + contactId + '.json';
+        $http.delete(url)
+        .success(function(data){
+          cb()
+        })
+        .error(function(err){
+          console.log('remove contact error:' + err);
+        });
+      }
+
+      return{
+        getContact: getContact,
+        editContact: editContact,
+        getAllContacts: getAllContacts,
+        createContact: createContact,
+        deleteContact: deleteContact
+      };
+    })
+
+    // ----- SHOW CONTROLLER -----
+    .controller('ShowController', function($routeParams, contactsFactory){
+      var vm = this;
+      var id = $routeParams.id;
+      contactsFactory.getContact(id, function(data){
+        vm.contact = data;
+      });
+    })
+
+    // ----- EDIT CONTROLLER -----
+    .controller('EditController', function($routeParams, contactsFactory){
+      var vm = this;
+      var id = $routeParams.id;
+
+      contactsFactory.getContact(id, function(data){
+        vm.newContact = data;
+      });
 
       vm.addNewContact = function(){
-        $http.post('https://leonaddress-book.firebaseio.com/contacts.json', vm.newContact)
-          .success(function(data){
-            console.log(vm.contacts);
-            console.log(vm.newContact);
-            vm.contacts[data.name] = vm.newContact;
-            vm.newContact = _defaultContact();
-          })
-          .error(function(err){
-            console.log('add contact error:' + err);
-          });
+        contactsFactory.editContact(id, vm.newContact)
+      };
+    })
+
+    // ----- ADD. BOOK CONTROLLER -----
+    .controller('addressBookController', function(contactsFactory){
+      var vm = this;
+
+      contactsFactory.getAllContacts(function(data){
+        vm.contacts = data;
+      })
+
+      // httpget();
+
+      vm.addNewContact = function(){
+        contactsFactory.createContact(vm.newContact, function(data){
+          vm.contacts[data.name] = vm.newContact;
+          vm.newContact = _defaultContact();
+        });
       };
 
       vm.removeContact = function(contactId){
-        var url = 'https://leonaddress-book.firebaseio.com/contacts/' + contactId + '.json';
-        $http.delete(url)
-          .success(function(data){
-            delete vm.contacts[contactId];
-          })
-          .error(function(err){
-            console.log('remove contact error:' + err);
-          });
+        contactsFactory.deleteContact(contactId, function(){
+          delete vm.contacts[contactId];
+        })
       };
 
       vm.newContact = _defaultContact();
@@ -106,6 +146,5 @@
           name: ''
         }
       }
-      // vm.newContact = {};
   });
 })();
